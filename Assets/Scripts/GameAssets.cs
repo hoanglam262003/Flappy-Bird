4d9d6ecaae7116d66a6f230ca4041258a0534331
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-10-09 16:50:56 +0700

    basic setup

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    private static GameAssets instance;
+    public static GameAssets GetInstance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public Sprite pipeHead;
+}
